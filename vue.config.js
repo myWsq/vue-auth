f05fca7cac839695fe7472568b88c775d4789399
帅琪王 <wsq@wsq.local>
2019-05-21 20:46:05 +0800

    chore: remove *.ts and change public url

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/vue-auth-example/" : "/",
+    process.env.NODE_ENV === "production" ? "/vue-auth/" : "/",
   outputDir: 'docs'
 };
