170ced3037822c193f21c40bfbf6a2e2521421ef
wsq <wsq961@outlook.com>
2019-05-21 21:35:34 +0800

    docs:

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/vue-router-guard/" : "/",
+    process.env.NODE_ENV === "production" ? "/vue-router-role/" : "/",
   outputDir: 'docs'
 };
